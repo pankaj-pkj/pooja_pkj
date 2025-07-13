5028751547babec816060a878fdc641a9bf48e6e
pankaj-pkj <pourwalpkj@gmail.com>
2025-07-13 22:51:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,11 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Render ke env se token le raha hai
 TOKEN = os.getenv("BOT_TOKEN")
 
-# /start command ka async handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Bot is running on Render 🚀")
 
-# Application setup
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
